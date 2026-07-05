ad5229499bc5be410b5807a0137109fb8043ff63
Alessandro Javier Olivares Velarde <adev-web@users.noreply.github.com>
2026-07-05 01:58:23 -0500

    feat: agrega presets de preparacion

---
+++ src/config/presets.js
@@ -0,0 +1,9 @@
+export const PRESETS = {
+  personalizado: { ratio: "", time: 0 },
+  frenchPress: { ratio: 15, time: 4 },
+  pourOver: { ratio: 16, time: 3.5 },
+  drip: { ratio: 17, time: 5 },
+  aeropress: { ratio: 15, time: 1.5 },
+  moka: { ratio: 7, time: 4 },
+  espresso: { ratio: 2, time: 0.5 },
+};
